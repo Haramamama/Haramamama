83843716ff9982115fb288d928606adc8165c4b7
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-20 10:04:43 +0900

    first commit

---
+++ testProject/src/test.java
@@ -0,0 +1,16 @@
+public class test {
+
+    public static void main (String[]args){
+
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.println("안녕하세요");
+        System.out.print("안녕");
+        System.out.print("안녕");
+        System.out.print("안녕");
+        System.out.print("안녕");
+    }
+}
+
+
+
