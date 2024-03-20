ee119b0bbeb47bcd9bc7bbcc65b5c324dacbe3a8
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-20 15:52:32 +0900

    seconf commit

--- 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section01/literal/Application3.java
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section01/literal/Application3.java
@@ -19,7 +19,7 @@ public class Application3 {
         System.out.println( 9 + "9" + 9 ); // 999 // 왼쪽의 연산부터 수행하고 수행결과를 오른쪽과 연산함.
         System.out.println( 9 + 9 + "9" ); // 189 // + 연산병합은 왼쪽에서 오른쪽이다. (숫자부터 합쳐짐)
 
-        System.out.println( "9" + (9 + 9) ); // 918 // 괄호먼저 연산한다.
+        System.out.println( "9" + (9 + 9) ); // 9                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    18 // 괄호먼저 연산한다.
 
         /* 목차3. 문자열 합치기 응용 */
         /* 10과 20의 사칙연산 결과물 출력하세요 */
@@ -35,11 +35,9 @@ public class Application3 {
         System.out.println( "10과 20의 차: " + (10-20)); // 문자열은 마이너스 연산이 불가능하기에 문자열과 마이너스는 연산이 불가능하다(반드시)
         System.out.println( "10과 20의 곱: " + (10*20)); //
         System.out.println( "10과 20의 나누기한 몫:" + (10/20));
-<<<<<<< HEAD
+
         System.out.println( "10과 20의 나누기한 나머지:" + (10%20) );
-=======
-        System.out.println( "10과 20의 나누기한 나머지:" + (10%20));
->>>>>>> a920316 (first commit)
+
 
         System.out.println("기차"); //기차
         System.out.println("기차" + "칙칙폭폭");//기차칙칙폭폭
---
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section02/variable/Application2.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.variable;
+
+public class Application2 {
+
+    public static void main(String[]args){
+
+        /* */
+    }
+}
