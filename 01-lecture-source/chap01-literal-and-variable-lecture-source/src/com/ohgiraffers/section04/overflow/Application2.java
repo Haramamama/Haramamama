0f8e9cea5433dd8d71b47aa8ae8a5df416bad8b1
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-21 12:40:54 +0900

    형변환

--- 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section04/overflow/Application2.java
+++ 01-lecture-source/chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section04/overflow/Application2.java
@@ -6,6 +6,44 @@ public class Application2 {
 
         /* 수업 목펴: 오버플로우에 대해 이해할 수 있다 */
         /* 자료형의 값의 최대 범위를 벗어나는 경우
-        * */
+        * 발생한 carry를 버림처리하고 sign bit를 반전시켜 최소값으로 순환시킴*/
+
+        /*1. 오버플로우 */
+        byte num1 = 127;
+
+        System.out.println("num1 :" + num1);
+
+        // num1 = num1+1; // 사용x
+        num1++; // 1이 증가
+
+        System.out.println("num1  오버플로우 :" + num1);
+
+        /*2. 언더플로우 */
+        /* 오버플로우와 반대의 개념으로 최소범위보다 작은 수를 발생시키는 경우 발생하는 현상이다. */
+
+        byte num2 = -128;
+
+        System.out.println("num2 :" + num2);
+
+        num2--; // 1이 감소
+
+        System.out.println("num2  언더플로우 :" + num2);
+
+        int firstNum = 1000000; // 100만
+        int secondNum = 700000; // 70만
+
+        int multi = firstNum * secondNum;  // 7천억이 나와야함
+
+        System.out.println("firstNum * secondNum = " +multi);
+
+        /* 3. 해결 방법*/
+        /* 오버플로우를 예측하고 더 큰 자료형으로 결과값을 받아서 처리한다. */
+        long longMulti = firstNum * secondNum;
+        System.out.println("firstNum * secondNum = " +multi);  // 똑같다.....
+
+        long result = (long)firstNum * secondNum; //(long) - 강제 형 변환
+
+        System.out.println("result : " + result);
+
     }
 }
