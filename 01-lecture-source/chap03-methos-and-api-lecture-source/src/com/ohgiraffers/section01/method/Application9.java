1bdb1eb541ea21fc3b356e05781a6de97ed75221
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-22 16:04:04 +0900

    커밋

--- 01-lecture-source/chap03-methos-and-api-lecture-source/src/com/ohgiraffers/section01/method/Application9.java
+++ 01-lecture-source/chap03-methos-and-api-lecture-source/src/com/ohgiraffers/section01/method/Application9.java
@@ -18,7 +18,7 @@ public class Application9 {
         * 사용할 이름.메소드명();*/
 
         Calculator cal = new Calculator(); // 동일한 패키지에 있기에 선언 가능
-        int min = cal.minNumberDf(first,second);
+        int min = cal.minNumberOf(first,second);
         System.out.println("두 수 중 최소값은? " + min);
 
         /* static 메소드인 경우 */
@@ -27,19 +27,19 @@ public class Application9 {
         * */
 
         // int max = maxNumberDf(first,second); // 클래스명을 생각하면 에러가 발생합니다.
-        int max = Calculator.maxNumberDf(first, second);
+        int max = Calculator.maxNumberOf(first, second);
 
         System.out.println("두 수 중 최대값은? : " + max);
 
         /*
-        * static 메소드는 non-static메소드처럼 호출은 가능하지만
+        * static 메소드는 non-static 메소드처럼 호출은 가능하지만
         * 권장하지는 않는다.
         * 이미 메모리에 로딩되어있는 static 메소드는 여러 객체가 공유하게 된다.
         * 그때 객체로 접근하게 되면 인스턴스가 가진 값으로 공유된 값에 예상치 못하는 동작을 유발할 수 있기 때문에
         * 사용을 제한해 달라는 경고다. 시스템이 복잡할수록 유지보수에 악영향을 준다.
         * */
 
-        int max2 = cal.maxNumberDf(first, second);
+        int max2 = cal.maxNumberOf(first, second);
 
         System.out.println("두 수 중 최대값은? : " + max2);
     }
--- 01-lecture-source/chap03-methos-and-api-lecture-source/src/com/ohgiraffers/section01/method/Calculator.java
+++ 01-lecture-source/chap03-methos-and-api-lecture-source/src/com/ohgiraffers/section01/method/Calculator.java
@@ -2,12 +2,12 @@ package com.ohgiraffers.section01.method;
 
 public class Calculator {
 
-    public int minNumberDf(int first, int second){       //non-static
+    public int minNumberOf(int first, int second){       //non-static
 
         return (first>second)? second : first;
     }
 
-    public static int maxNumberDf(int first, int second){
+    public static int maxNumberOf(int first, int second){
 
         return (first>second)? first : second;
     }
