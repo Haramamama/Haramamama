b1c06ab3c51e6f0f8871152c62e0dd6b4ed42cba
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-25 18:03:32 +0900

    커밋

---
+++ 01-lecture-source/chap04-control-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application1.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        //A_for a = new A_for();
+       // a.testSimpleForStatement();
+        //a.testForExample1();
+        //a.testForExample2();
+        //a.printSimplegugudan();
+
+        A_nestedFor an = new A_nestedFor();
+        //an.printGugudanFromTwoToNice();
+        //an.printStarInputRowTimes();
+        //an.printTriangleStars();
+
+        B_while b = new B_while();
+        //b.testWhileEcample();
+       // b.testWhileEcample2();
+
+        C_doWhile c = new C_doWhile();
+        c.testSimpleDowhileStatment();
+
+
+    }
+}
