e8bfe1f7e203f89a1a375d19c0d9c3fd17b08080
DESKTOP-F4BC13L\hi <a48219441@gmail.com>
2024-03-26 11:20:04 +0900

    커밋

---
+++ 01-lecture-source/chap04-control-flow-lecture-source/src/com/ohgiraffers/section03/branching/Application1.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section03.branching;
+
+public class Application1 {
+
+    public static void main(String[] args) { // 분기문을 호출하기 위한 메소드
+
+        A_break a = new A_break();
+        //a.testSimpleBreakStatement();
+        //a.testSimpleBreakStatement2();
+
+
+        B_continue b = new B_continue();
+        b.testSimpleContinueStatement();
+
+    }
+
+}
